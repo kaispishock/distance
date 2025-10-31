b1957a8b4fda78e9363eb4bb1abbe2a24e8ef846
Kai Spishock <kaispishock@sdf.org>
2025-10-31 13:06:35 -0700

    Update main.c

--- main.c
+++ main.c
@@ -9,28 +9,28 @@ float distCalc(float a, float b, float c, float d);
 
 // main function - handles the arguments
 int main(int argc, char * argv[]) {
-	if (argv[1]="-h","--help") {	// at least the user admits they're dumb.
-		printHelp(); // tell 'em how to do shit.
-		return 0; // nicely exit, because the user actually asked for help instead of just failing miserably.
-	} else if (argc=4) {	// not printing help, time to actualy do work.
+	if (argv[1]="-h","--help") {
+		printHelp();
+		return 0;
+	} else if (argc=4) {
 		float x1,x2,y1,y2;	// make the vars.
-		x1=atof(argv[1]);	// convert the args from stings into floats so we can use them as arguments for the distcalc func.
+		x1=atof(argv[1]);	// take float from string, and use it to define vars.
 		x2=atof(argv[2]);
 		y1=atof(argv[3]);
 		y2=atof(argv[4]);
-		distCalc(x2,x1,y2,y1);	// actualy invoke the function
-		return 0;	// gtfo
-	} else {	// dumbass.
-		printf("Invalid number of arguments (exit code 1)/n"); // tell the user they are dumb.
-		printHelp(); // tell the user how to do shit
-		return 1; // exit with an error code of 1, as a metaphorical slap on the ass.
+		distCalc(x2,x1,y2,y1);	// calculate distance
+		return 0;	// exit
+	} else {
+		printf("Invalid number of arguments (exit code 1)/n");
+		printHelp();
+		return 1;
 	}
 }
-float distCalc(float a, float b, float c, float d) { // actualy calculate the distance
+float distCalc(float a, float b, float c, float d) { // distance calculation function.
 	float e=a-b; float f=c-d;			// incredibly cringe solution.
 	e=powf(e, 2.0f); f=powf(f, 2.0f); 	// should have to only use 4 vars for 4 numbers, but here we are.
 	printf("%f/n", sqrt(e+f));	// dumbass output
-	return 0;	// finaly exit.
+	return 0;	// exit.
 }
 int printHelp() {
   printf("usage: distance [-h] [1] [2] [3] [4]"); // howto format help?
