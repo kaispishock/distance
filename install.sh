d2098a53cbb3a121f531b1dd47e434172f297685
Kai Spishock <kaispishock@sdf.org>
2025-11-25 09:45:48 -0700

    Delete install.sh
    Postpone implementation.

--- install.sh
+++
@@ -1,7 +0,0 @@
-# bin/sh
-# invaseive ass unix installer for kaispishock/math. 
-# run as root for maximum fuckage.
-echo "installing to /usr/bin..."
-gcc /geometry/distance.c -o /usr/bin/distance
-gcc /algebra/quadratic.c -o /usr/bin/quadratic
-echo "done."
